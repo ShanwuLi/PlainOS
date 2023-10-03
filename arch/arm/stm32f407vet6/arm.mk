538f7ae54cf6426a38ab241881b71cc8e057b0d4
lsw_elecm <1508555728@qq.com>
2023-10-03 22:36:40 +0800

    add atmega128 build options

---
+++ arch/arm/stm32f407vet6/arm.mk
@@ -0,0 +1,19 @@
+# compiler flags
+DEFINE   += -DSTM32F40X_MD -D__CHECK_DEVICE_DEFINES -D__CM4_REV
+C_FLAGS  += $(DEFINE) -mthumb -Wall --specs=nosys.specs -Wextra -Wwrite-strings -Wformat=2 \
+           -Werror=format-nonliteral -Wvla -Wlogical-op -Wshadow -Wformat-signedness \
+           -Wformat-overflow=2 -Wformat-truncation -Werror -Wmissing-declarations \
+           -fdiagnostics-color=always -ffunction-sections -fdata-sections -Wall \
+           -Werror=all -Werror=unused-function -Werror=unused-variable -Werror=deprecated-declarations \
+           -Wextra -Werror=unused-parameter -Werror=sign-compare  -gdwarf-4 -ggdb -nostartfiles \
+           -fstrict-volatile-bitfields -Werror=unused-but-set-variable -fno-jump-tables \
+           -fno-tree-switch-conversion
+CC_FLAGS  += $(C_FLAGS) -xc -Wmissing-prototypes -Werror=old-style-declaration \
+                            -std=c11
+CXX_FLAGS += $(C_FLAGS) -xc++ -std=c++14
+ASM_FLAGS += -x assembler-with-cpp
+LDFLAGS   += -Wl,--gc-sections,--print-memory-usage -specs=nano.specs
+LIBS      += -lc -lm -lnosys
+
+INC += -I$(TOPDIR)/arch/arm/stm32f407vet6
+INC += -I$(TOPDIR)/arch/arm/stm32f407vet6/cmsis
