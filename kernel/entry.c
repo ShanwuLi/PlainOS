2f0880ce5792b2cb05f331e50bbd8ad4b2eede2c
Grenaf <310116595@qq.com>
2024-03-14 23:22:53 +0800

    Updating MakeFile and kernel/entry.c file

--- kernel/entry.c
+++ kernel/entry.c
@@ -1,1 +1,18 @@
 #include <types.h>
+
+void pl_callee_entry(void)
+{
+	int ret;
+	pl_syslog_init();
+	ret = pl_port_putc_init();
+	if (ret < 0)
+		while(1);
+
+	pl_default_mempool_init();
+	pl_do_early_initcalls();
+	pl_do_initcalls();
+	pl_task_core_init();
+	pl_idle_task_init();
+	while(1);
+}
+
