36e268918ea6e4fd4525a6e975b344a18948c0c5
Grenaf <310116595@qq.com>
2024-03-17 17:06:08 +0800

    unfinished V1.0

--- arch/arm32/stm32f103c8t6/early_setup/early_uart.c
+++ arch/arm32/stm32f103c8t6/early_setup/early_uart.c
@@ -5,29 +5,29 @@
 
 void USART1_Init(uint_t USART1_BaudRate)
 {
-   unsigned int integer;                   //����һ���������洢��������
-   float   decimal;                        //����һ���������洢С������
-   RCC->APB2ENR|=(1<<14)|(1<<2);           //����USART1ʱ�Ӻ�GPIOAʱ��
-	 RCC->APB2RSTR|=(1<<14);                 //��λ����USART1
-	 RCC->APB2RSTR&=(~(1<<14));              //ֹͣ��λ
+   unsigned int integer;                  
+   float   decimal;                       
+   RCC->APB2ENR|=(1<<14)|(1<<2);          
+	RCC->APB2RSTR|=(1<<14);                
+	RCC->APB2RSTR&=(~(1<<14));            
 	 
-	 GPIOA->CRH&=0XFFFFF00F;                 //���֮ǰ����PA10��PA9
-	 GPIOA->CRH|=0X000008B0;                 //����PA10Ϊ�������룬����PA9Ϊ�����������
+	GPIOA->CRH&=0XFFFFF00F;                
+	GPIOA->CRH|=0X000008B0;                
 	
 	
-   USART1->CR1|=(1<<13);                   //ʹ��USARTģ��
-   USART1->CR1|=(1<<2);                    //ʹ�ܽ���
-	 USART1->CR1|=(1<<5);                    //���ж�
-   USART1->CR1|=(1<<3);                    //ʹ�ܷ���	
+   USART1->CR1|=(1<<13);                  
+   USART1->CR1|=(1<<2);                  
+	USART1->CR1|=(1<<5);                  
+   USART1->CR1|=(1<<3);                    
 	 
 	
-   integer=72*1000*1000/(USART1_BaudRate*16);         //ȡ����������
-   decimal=(float)(72*1000*1000/(USART1_BaudRate*16))-integer;   //ȡ��С������
-   USART1->BRR=(integer<<4)|((unsigned int)decimal*16);  //��ת�����ֵ����BRR�Ĵ���
+   integer=72*1000*1000/(USART1_BaudRate*16);         
+   decimal=(float)(72*1000*1000/(USART1_BaudRate*16))-integer;   
+   USART1->BRR=(integer<<4)|((unsigned int)decimal*16);  
    
-   NVIC_SetPriorityGrouping(1);            //�������ȼ�����1
-   NVIC_SetPriority(USART1_IRQn, NVIC_EncodePriority(1,2,1));     //������ռ���ȼ�Ϊ1�������ȼ�Ϊ1
-   NVIC_EnableIRQ(USART1_IRQn);            //ʹ��USART�ж�
+   NVIC_SetPriorityGrouping(1);            
+   NVIC_SetPriority(USART1_IRQn, NVIC_EncodePriority(1,2,1));     
+   NVIC_EnableIRQ(USART1_IRQn);            
 }
 
 int USART1_PrintChar(char c)
@@ -39,5 +39,5 @@ int USART1_PrintChar(char c)
 		if(USART1->SR & (1 << 6))
 			return 0;
 
-	return -ETIMEOUT;
+	return -1;
 }
--- kernel/entry.c
+++ kernel/entry.c
@@ -1,18 +1,11 @@
 #include <types.h>
+#include <early_setup\early_uart.h>
 
+void pl_callee_entry(void);
 void pl_callee_entry(void)
 {
-	int ret;
-	pl_syslog_init();
-	ret = pl_port_putc_init();
-	if (ret < 0)
-		while(1);
-
-	pl_default_mempool_init();
-	pl_do_early_initcalls();
-	pl_do_initcalls();
-	pl_task_core_init();
-	pl_idle_task_init();
-	while(1);
+	while(1){
+		USART1_PrintChar('A');
+	};
 }
 
