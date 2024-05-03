f0ac7ade26dc43b438bfe3000ca587fcd4208569
Grenaf <310116595@qq.com>
2024-05-03 22:01:45 +0800

    "Moditying task function code"

--- chapter2/chapter2.2/kernel/entry.c
+++ chapter2/chapter2.2/kernel/entry.c
@@ -10,13 +10,15 @@
 #define Task04StackSize 100
 #define Task05StackSize 100
 #define Task06StackSize 100
-#define TaskNumbers		6
+#define Task07StackSize 100
+#define TaskNumbers		7
 static uint32_t Task01Stack[Task01StackSize];
 static uint32_t Task02Stack[Task02StackSize];
 static uint32_t Task03Stack[Task03StackSize];
 static uint32_t Task04Stack[Task04StackSize];
 static uint32_t Task05Stack[Task05StackSize];
 static uint32_t Task06Stack[Task06StackSize];
+static uint32_t Task07Stack[Task07StackSize];
 
 //创建任务控制块记录任务堆栈指针变化情况
 typedef struct TaskControlBlock{
@@ -25,11 +27,11 @@ typedef struct TaskControlBlock{
 static TCB TaskControlBlockArray[TaskNumbers];
 
 //首次运行任务内容前需要将MCU状态送入各自的任务栈空间内，void (*Task)(void) Task变量指向函数，此处形式参数用于传递函数地址
-static void  TaskCreate(void (*Task)(void), uint32_t *TaskStack, 
-				uint32_t StackSize, TCB *TaskControlBlock);
+static void  TaskCreate(void (*Task)(void *arg), uint32_t *TaskStack, 
+				uint32_t StackSize, TCB *TaskControlBlock, void *arg);
 //保存Context的过程参考Cortex-M3权威指南9.1中断/异常的响应序列，入栈顺序为表9.1
-static void  TaskCreate(void (*Task)(void), uint32_t *TaskStack, 
-				uint32_t StackSize, TCB *TaskControlBlock)
+static void  TaskCreate(void (*Task)(void *arg), uint32_t *TaskStack, 
+				uint32_t StackSize, TCB *TaskControlBlock, void *arg)
 {
 	uint32_t *stack = (uint32_t *)TaskStack;
 	stack = stack + StackSize; //为了方便理解这里将任务栈存储空间设置为Nx32结构和STM32寄存器位数保持一样，确保SP入栈一次减少4,stack变量减少1
@@ -41,7 +43,9 @@ static void  TaskCreate(void (*Task)(void), uint32_t *TaskStack,
 	*(--stack)  = (u32_t)0;        /* R3 */
 	*(--stack)  = (u32_t)0;        /* R2 */
 	*(--stack)  = (u32_t)0;        /* R1 */
-	*(--stack)  = (u32_t)0;    	   /* R0 */
+	*(--stack)  = (u32_t)arg;      /* R0 */
+
+
 
 	*(--stack)  = (u32_t)0;        /* R4 */	//这里入栈顺序依照用户喜欢的顺序，这里就按照寄存器名称顺序入栈
 	*(--stack)  = (u32_t)0;        /* R5 */
@@ -73,97 +77,26 @@ void *pl_callee_get_next_context_sp(void)
 
 void pl_port_switch_context(void);
 
-void task01(void);
-void task02(void);
-void task03(void);
-void task04(void);
-void task05(void);
-void task06(void);
 void delay(uint32_t n);
-
-void task01(void){
-	while(1){
-		USART1_PrintChar('T');
-		USART1_PrintChar('A');
-		USART1_PrintChar('S');
-		USART1_PrintChar('K');
-		USART1_PrintChar('1');
-		USART1_PrintChar('\t');
-		delay(1000000);
-		pl_port_switch_context();	//任务中循环调用PendSV
-	};
-}
-
-void task02(void){
-	while(1){
-		USART1_PrintChar('T');
-		USART1_PrintChar('A');
-		USART1_PrintChar('S');
-		USART1_PrintChar('K');
-		USART1_PrintChar('2');
-		USART1_PrintChar('\t');
-		delay(1000000);
-		pl_port_switch_context();	//任务中循环调用PendSV
-	};
-}
-
-void task03(void){
-	while(1){
-		USART1_PrintChar('T');
-		USART1_PrintChar('A');
-		USART1_PrintChar('S');
-		USART1_PrintChar('K');
-		USART1_PrintChar('3');
-		USART1_PrintChar('\t');
-		delay(1000000);
-		pl_port_switch_context();	//任务中循环调用PendSV
-	};
-}
-
-void task04(void){
-	while(1){
-		USART1_PrintChar('T');
-		USART1_PrintChar('A');
-		USART1_PrintChar('S');
-		USART1_PrintChar('K');
-		USART1_PrintChar('4');
-		USART1_PrintChar('\t');
-		delay(1000000);
-		pl_port_switch_context();	//任务中循环调用PendSV
-	};
-}
-
-void task05(void){
-	while(1){
-		USART1_PrintChar('T');
-		USART1_PrintChar('A');
-		USART1_PrintChar('S');
-		USART1_PrintChar('K');
-		USART1_PrintChar('5');
-		USART1_PrintChar('\t');
-		delay(1000000);
-		pl_port_switch_context();	//任务中循环调用PendSV
-	};
-}
-
-void task06(void){
-	while(1){
-		USART1_PrintChar('T');
-		USART1_PrintChar('A');
-		USART1_PrintChar('S');
-		USART1_PrintChar('K');
-		USART1_PrintChar('6');
-		USART1_PrintChar('\t');
-		delay(1000000);
-		pl_port_switch_context();	//任务中循环调用PendSV
-	};
-}
-
 void delay(uint32_t n){
 	u32_t volatile i = 0;
 	for(i=0; i < n; ){
 		i = i + 1;
 	}
+}
+
+void task(void *arg);
+void task(void *arg){
+	while(1){
+		USART1_PrintChar('T');
+		USART1_PrintChar('A');
+		USART1_PrintChar('S');
+		USART1_PrintChar('K');
+		USART1_PrintChar((char)((uint32_t)arg));
+		USART1_PrintChar('\t');
+		delay(1000000);
+		pl_port_switch_context();	//任务中循环调用PendSV
+	};
 }
 
 void pl_callee_entry(void);
@@ -172,12 +105,13 @@ void pl_callee_entry(){
 	USART1_Init(USART1_BaudRate);
 	USART1_PrintChar('S');
 	USART1_PrintChar('\t');
-	TaskCreate(task01, Task01Stack, Task01StackSize, &TaskControlBlockArray[0]);
-	TaskCreate(task02, Task02Stack, Task02StackSize, &TaskControlBlockArray[1]);
-	TaskCreate(task03, Task03Stack, Task03StackSize, &TaskControlBlockArray[2]);
-	TaskCreate(task04, Task04Stack, Task04StackSize, &TaskControlBlockArray[3]);
-	TaskCreate(task05, Task05Stack, Task05StackSize, &TaskControlBlockArray[4]);
-	TaskCreate(task06, Task06Stack, Task06StackSize, &TaskControlBlockArray[5]);
+	TaskCreate(task, Task01Stack, Task01StackSize, &TaskControlBlockArray[0], (void *)'1');
+	TaskCreate(task, Task02Stack, Task02StackSize, &TaskControlBlockArray[1], (void *)'2');
+	TaskCreate(task, Task03Stack, Task03StackSize, &TaskControlBlockArray[2], (void *)'3');
+	TaskCreate(task, Task04Stack, Task04StackSize, &TaskControlBlockArray[3], (void *)'4');
+	TaskCreate(task, Task05Stack, Task05StackSize, &TaskControlBlockArray[4], (void *)'5');
+	TaskCreate(task, Task06Stack, Task06StackSize, &TaskControlBlockArray[5], (void *)'6');
+	TaskCreate(task, Task07Stack, Task07StackSize, &TaskControlBlockArray[6], (void *)'7');
 	pl_port_switch_context();	//第一次手动调用PendSV
 	while(1);
 }
